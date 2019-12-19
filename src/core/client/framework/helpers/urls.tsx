594aeec17c842fe5f7485554c91e7c2910421e78
Wyatt Johnson <me@wyattjoh.ca>
2019-12-19 14:54:47 -0700

    fix: review comments to adjuist flow

--- src/core/client/framework/helpers/urls.tsx
+++ src/core/client/framework/helpers/urls.tsx
@@ -1,7 +1,9 @@
 export default {
   admin: {
     moderateReported: "/admin/moderate/reported",
-    configureWebhooks: "/admin/configure/webhooks",
+    webhooks: "/admin/configure/webhooks",
+    addWebhookEndpoint: "/admin/configure/webhooks/add",
+    configureWebhookEndpoint: "/admin/configure/webhooks/endpoint",
   },
   embed: {
     stream: "/embed/stream",
